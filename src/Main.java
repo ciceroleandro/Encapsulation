023b9a7db6e20ebe4cc308060becce1418696a7d
ciceroleandro@gmail.com <ciceroleandro@gmail.com>
2023-05-09 22:36:36 -0300

    Encapsulation

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.pause();
+        c.menosVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+
+
+    }
+}
